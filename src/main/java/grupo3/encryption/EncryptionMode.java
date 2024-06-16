5004add61fc03ca18d2bec84842a912d72d0cb13
ichayer <ichayer@itba.edu.ar>
2024-06-16 00:44:23 -0300

    Add documentation in EncryptionMode enum

--- src/main/java/grupo3/encryption/EncryptionMode.java
+++ src/main/java/grupo3/encryption/EncryptionMode.java
@@ -1,27 +1,31 @@
 package grupo3.encryption;
 
 /**
- * Lists the supported block cipher primitives.
+ * Lists the supported block cipher modes for encryption.
  */
 public enum EncryptionMode {
 
     /**
-     * Indicates Electronic Code Book Mode.
+     * Electronic Code Book Mode (ECB).
+     * Does not use IV, supports padding.
      */
     ECB("ECB", true, false),
 
     /**
-     * Indicates Cipher Block Chaining.
+     * Cipher Block Chaining Mode (CBC).
+     * Uses IV, supports padding.
      */
     CBC("CBC", true, true),
 
     /**
-     * Indicates Cipher Feedback Mode.
+     * Cipher Feedback Mode (CFB).
+     * Uses IV, does not support padding.
      */
     CFB("CFB8", false, true),
 
     /**
-     * Indicates Output Feedback Mode.
+     * Output Feedback Mode (OFB).
+     * Uses IV, does not support padding.
      */
     OFB("OFB", false, true);
 
@@ -29,20 +33,42 @@ public enum EncryptionMode {
     private final boolean usePadding;
     private final boolean useIV;
 
+    /**
+     * Constructs an EncryptionMode enum instance with the specified parameters.
+     *
+     * @param mode       The name of the encryption mode.
+     * @param usePadding Indicates if padding is used in this mode.
+     * @param useIV      Indicates if an initialization vector (IV) is used in this mode.
+     */
     EncryptionMode(String mode, boolean usePadding, boolean useIV) {
         this.mode = mode;
         this.usePadding = usePadding;
         this.useIV = useIV;
     }
 
+    /**
+     * Retrieves the name of the encryption mode.
+     *
+     * @return The encryption mode name.
+     */
     public String getMode() {
         return mode;
     }
 
+    /**
+     * Checks if padding is used in this encryption mode.
+     *
+     * @return True if padding is used, false otherwise.
+     */
     public boolean usePadding() {
         return usePadding;
     }
 
+    /**
+     * Checks if an initialization vector (IV) is used in this encryption mode.
+     *
+     * @return True if IV is used, false otherwise.
+     */
     public boolean useIV() {
         return useIV;
     }
