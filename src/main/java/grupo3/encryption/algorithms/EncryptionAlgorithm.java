725212514ad1830b34d9bd90cced0348cf1ca871
ichayer <ichayer@itba.edu.ar>
2024-06-17 14:47:18 -0300

    Add byteArrayToHexString method in EncryptionAlgorithm for debugging purposes

--- src/main/java/grupo3/encryption/algorithms/EncryptionAlgorithm.java
+++ src/main/java/grupo3/encryption/algorithms/EncryptionAlgorithm.java
@@ -168,4 +168,25 @@ public abstract class EncryptionAlgorithm {
     private int bytesToBits(int bytes) {
         return bytes * Byte.SIZE;
     }
+
+    /**
+     * Converts a byte array to a hexadecimal string representation.
+     * This method is specifically designed for debugging purposes to display
+     * the hexadecimal values of the IV (Initialization Vector) and derived KEY.
+     *
+     * @param bytes The byte array to be converted.
+     * @return A string containing hexadecimal representation of the byte array.
+     */
+
+    // https://stackoverflow.com/questions/9655181/java-convert-a-byte-array-to-a-hex-string
+    private static final byte[] HEX_ARRAY = "0123456789ABCDEF".getBytes(StandardCharsets.US_ASCII);
+    public static String bytesToHex(byte[] bytes) {
+        byte[] hexChars = new byte[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+        }
+        return new String(hexChars, StandardCharsets.UTF_8);
+    }
 }
